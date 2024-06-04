cfcb15c015407a6596b131e9d4551f7a5ec6482d
Max <punkimen666@gmail.com>
2024-06-04 11:09:56 +0300

    base repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  base: "/MadamRoche/",
   resolve: {
     alias: {
       "@": path.resolve("src"),
